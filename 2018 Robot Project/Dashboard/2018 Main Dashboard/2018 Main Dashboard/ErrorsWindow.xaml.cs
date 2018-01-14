3a9bc8b77ef5aed18f7649efc1b91af420c2b068
Nick <themandolorian2000@gmail.com>
2018-01-14 14:27:00 -0500

    Set up basic layout for caution box and error window

--- 2018 Robot Project/Dashboard/2018 Main Dashboard/2018 Main Dashboard/ErrorsWindow.xaml.cs
+++ 2018 Robot Project/Dashboard/2018 Main Dashboard/2018 Main Dashboard/ErrorsWindow.xaml.cs
@@ -10,25 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace _2018_Main_Dashboard
 {
     /// <summary>
-    /// Interaction logic for AlertBox.xaml
+    /// Interaction logic for ErrorsWindow.xaml
     /// </summary>
-    public partial class AlertBox : UserControl
+    public partial class ErrorsWindow : Window
     {
-        public AlertBox()
+        public ErrorsWindow()
         {
             InitializeComponent();
-        }
-
-        private void Swap_Button_Click(object sender, RoutedEventArgs e)
-        {
-            ErrorsWindow ew = new ErrorsWindow();
-            ew.Show();
         }
     }
 }
