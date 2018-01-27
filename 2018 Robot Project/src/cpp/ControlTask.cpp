2b07ca10372e532f7810013f6258807492d75e1e
CrapTopOG\Matthew <matthewiasevoli@gmail.com>
2018-01-27 14:59:45 -0500

    Control Task for double joystick switches completed

--- 2018 Robot Project/src/cpp/ControlTask.cpp
+++ 2018 Robot Project/src/cpp/ControlTask.cpp
@@ -41,7 +41,24 @@ void ControlTask::Always()
 	{
 		Joystick * SwitchesLeft = new Joystick(2);
 		Joystick * SwitchesRight = new Joystick(3);
-		if (SwitchesLeft->GetRawAxis(0))
+		if (SwitchesLeft->GetRawAxis(1) >= 0.25)
+		{
+			Controls->SpeedLift = 1.25 * (SwitchesLeft->GetRawAxis(1) - 0.25);
+		}
+		else if (SwitchesLeft->GetRawAxis(1) <= -0.25)
+		{
+			Controls->SpeedLift = 1.25 * (SwitchesLeft->GetRawAxis(1) + 0.25);
+		}
+		if (SwitchesRight->GetRawAxis(1) >= 0.25)
+		{
+			Controls->SpeedLift = Controls->SpeedLift - 1.25 * (SwitchesRight->GetRawAxis(1) - 0.25);
+		}
+		else if (SwitchesRight->GetRawAxis(1) <= -0.25)
+		{
+			Controls->SpeedLift = Controls->SpeedLift - 1.25 * (SwitchesRight->GetRawAxis(1) + 0.25);
+		}
+		Controls->SpeedLift = Controls->SpeedLift >= 0.99 ? 0.99 : Controls->SpeedLift;
+		Controls->SpeedLift = Controls->SpeedLift <= -0.99 ? -0.99 : Controls->SpeedLift;
 		Controls->SpeedGrabWheelLeft = SwitchesLeft->GetRawButton(0) - SwitchesLeft->GetRawButton(1);
 		Controls->SpeedGrabWheelRight = SwitchesRight->GetRawButton(0) - SwitchesRight->GetRawButton(1);
 		Controls->SpeedArmLeft = SwitchesLeft->GetRawAxis(2);
