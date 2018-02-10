8c89332eca58b4bf909d856cee12c0662aad6a49
Michael Todd <eltodd@gmail.com>
2018-02-10 09:47:55 -0500

    Switch current reporting to 1s running average instead of momentary current

--- 2018 Robot Project/src/cpp/PowerReportingTask.cpp
+++ 2018 Robot Project/src/cpp/PowerReportingTask.cpp
@@ -14,12 +14,20 @@ void PowerReportingTask::Disable()
 
 }
 
+double AveragePowerValues[16];
+
 void PowerReportingTask::Always()
 {
 	for (int i = 0; i < 16; i++)
 	{
-		MaxLog::TransmitDouble("/power/" + std::to_string(i), PDP->GetCurrent(i));
+		AveragePowerValues[i] = (((AveragePowerValues[i] * 20.0) - AveragePowerValues[i]) + PDP->GetCurrent(i)) / 20.0;
 	}
+
+	for (int i = 0; i < 16; i++)
+	{
+		MaxLog::TransmitDouble("/power/" + std::to_string(i), AveragePowerValues[i]);
+	}
+
 	MaxLog::TransmitDouble("/Power/Total/", PDP->GetTotalCurrent());
 }
 
@@ -36,4 +44,8 @@ void PowerReportingTask::ControllerUpdate(MaxControl * controls)
 void PowerReportingTask::Init()
 {
 	PDP = new PowerDistributionPanel();
+	for (int i = 0; i < 16; i++)
+	{
+		AveragePowerValues[i] = 0;
+	}
 }
