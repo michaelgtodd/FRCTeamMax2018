2b97995fbfd0cb84642cd4ddc576821417bc0004
Nick-PC\Nick <themandolorian2000@gmail.com>
2018-02-12 20:55:57 -0500

    music player/ theme selector start. weird bug where you cant view the prieview, but it still builds.

---
+++ 2018 Robot Project/Dashboard/2018 Main Dashboard/2018 Main Dashboard/AudioStuff.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _2018_Main_Dashboard
+{
+    /// <summary>
+    /// Interaction logic for AudioStuff.xaml
+    /// </summary>
+    public partial class AudioStuff : UserControl
+    {
+        SoundPlayer Music = new SoundPlayer("Music/Sanic.wav");
+        
+
+        public AudioStuff()
+        {
+            InitializeComponent();
+            Music.Load();
+            Music.Play();
+        }
+
+        private void Mute(object sender, RoutedEventArgs e)
+        {
+            Music.Stop();
+        }
+
+        private void Play(object sender, RoutedEventArgs e)
+        {
+            Music.Play();
+        }
+    }
+}
